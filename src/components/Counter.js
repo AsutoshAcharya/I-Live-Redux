908186e8b7235fb3367da173d872609fb5ed247d
Asutosh Acharya <asutosh246@gmail.com>
2022-11-16 17:16:36 +0530

    adding payload to action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -16,12 +16,17 @@ const Counter = () => {
     });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
