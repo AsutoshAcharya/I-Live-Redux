a58e4cd3ed7b2571ede36d6f48f0ccb1a4fd095c
AsutoshAcharya <asutosh246@gmail.com>
2022-12-26 16:28:30 +0530

    changes in slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,12 @@
 // import { legacy_createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
-import counterReducer from './counter-slice';
-import authReducer from './auth';
+import counterReducers from "./counter-slice";
+import authReducers from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterReducer, auth: authReducer },
+  reducer: { counter: counterReducers, auth: authReducers },
 });
-
 
 export default store;
 
